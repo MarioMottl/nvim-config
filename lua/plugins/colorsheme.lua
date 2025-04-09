8150ff68561cfc058a09c11d546541b9727463ff
Mario Mottl <mariomottl17@gmail.com>
2025-04-09 22:38:27 +0200

    add a switch between catpuccin and rose-pine colorsheme

--- lua/plugins/colorsheme.lua
+++ lua/plugins/colorsheme.lua
@@ -1,28 +1,52 @@
-return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                flavour = "mocha", -- Options: latte, frappe, macchiato, mocha
-                integrations = {
-                    treesitter = true,
-                    native_lsp = {
-                        enabled = true,
+-- Set this variable to either "catppuccin" or "rose-pine"
+local selected_colorscheme = "rose-pine" -- change as needed
+
+if selected_colorscheme == "catppuccin" then
+    return {
+        {
+            "catppuccin/nvim",
+            name = "catppuccin",
+            priority = 1000,
+            config = function()
+                require("catppuccin").setup({
+                    flavour = "mocha", -- Options: latte, frappe, macchiato, mocha
+                    integrations = {
+                        treesitter = true,
+                        native_lsp = {
+                            enabled = true,
+                        },
+                        telescope = true,
+                        cmp = true,
+                        gitsigns = true,
+                        nvimtree = true,
                     },
-                    telescope = true,
-                    cmp = true,
-                    gitsigns = true,
-                    nvimtree = true,
-                },
-            })
-        end,
-    },
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "catppuccin",
+                })
+            end,
         },
-    },
-}
+        {
+            "LazyVim/LazyVim",
+            opts = {
+                colorscheme = "catppuccin",
+            },
+        },
+    }
+elseif selected_colorscheme == "rose-pine" then
+    return {
+        {
+            "rose-pine/neovim",
+            name = "rose-pine",
+            priority = 1000,
+            config = function()
+                require("rose-pine").setup({
+                    dark_variant = "main", -- or "moon"
+                })
+            end,
+        },
+        {
+            "LazyVim/LazyVim",
+            opts = {
+                colorscheme = "rose-pine",
+            },
+        },
+    }
+end

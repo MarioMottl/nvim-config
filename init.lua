efe199f921cf64260da11415e74b515172929932
MarioMottl <mariomottl17@gmail.com>
2025-04-13 20:59:55 +0200

    update to melange

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.opt.clipboard = "unnamedplus"
+vim.o.number = true
+vim.o.relativenumber = false
+vim.opt.list = true
--- lua/plugins/colorsheme.lua
+++ lua/plugins/colorsheme.lua
@@ -1,52 +1,16 @@
--- Set this variable to either "catppuccin" or "rose-pine"
-local selected_colorscheme = "rose-pine" -- change as needed
-
-if selected_colorscheme == "catppuccin" then
-    return {
-        {
-            "catppuccin/nvim",
-            name = "catppuccin",
-            priority = 1000,
-            config = function()
-                require("catppuccin").setup({
-                    flavour = "mocha", -- Options: latte, frappe, macchiato, mocha
-                    integrations = {
-                        treesitter = true,
-                        native_lsp = {
-                            enabled = true,
-                        },
-                        telescope = true,
-                        cmp = true,
-                        gitsigns = true,
-                        nvimtree = true,
-                    },
-                })
-            end,
+return {
+    {
+        "savq/melange-nvim",
+        name = "melange",
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme melange")
+        end,
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "melange",
         },
-        {
-            "LazyVim/LazyVim",
-            opts = {
-                colorscheme = "catppuccin",
-            },
-        },
-    }
-elseif selected_colorscheme == "rose-pine" then
-    return {
-        {
-            "rose-pine/neovim",
-            name = "rose-pine",
-            priority = 1000,
-            config = function()
-                require("rose-pine").setup({
-                    dark_variant = "main", -- or "moon"
-                })
-            end,
-        },
-        {
-            "LazyVim/LazyVim",
-            opts = {
-                colorscheme = "rose-pine",
-            },
-        },
-    }
-end
+    },
+}

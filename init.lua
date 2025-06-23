5c432177ef43c67d85578299c1623a073b6f8e7d
Mario Mottl <github.wrongness914@passmail.net>
2025-06-23 22:43:03 +0200

    feat: add visual-mode p/P mappings to preserve system clipboard

--- init.lua
+++ init.lua
@@ -4,3 +4,5 @@ vim.opt.clipboard = "unnamedplus"
 vim.o.number = true
 vim.o.relativenumber = false
 vim.opt.list = true
+vim.keymap.set("x", "p", '"_dP', { noremap = true, silent = true })
+vim.keymap.set("x", "P", '"_dP', { noremap = true, silent = true })

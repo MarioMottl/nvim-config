a13ca89bd968b94695f34d8c07896320f4f5b9cc
MarioMottl <mario.mottl@pm.me>
2025-07-11 12:09:21 +0200

    feat: update packages / add theme-switcher / enable hidden files in explorer

--- lua/plugins/colorsheme.lua
+++ lua/plugins/colorsheme.lua
@@ -1,23 +1,47 @@
--- "zen" "ink" "pearl"
+local active_theme = "naysayer"
+
+-- kanso variants: "zen", "ink", "pearl"
 local kanso_variant = "zen"
 
-return {
+if active_theme == "kanso" then
+  return {
     {
-        "webhooked/kanso.nvim",
-        name = "kanso",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("kanso").setup({
-                theme = kanso_variant,
-            })
-            vim.cmd("colorscheme kanso-" .. kanso_variant)
-        end,
+      "webhooked/kanso.nvim",
+      name = "kanso",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        require("kanso").setup({
+          theme = kanso_variant,
+        })
+        vim.cmd("colorscheme kanso-" .. kanso_variant)
+      end,
     },
     {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "kanso-" .. kanso_variant,
-        },
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "kanso-" .. kanso_variant,
+      },
     },
-}
+  }
+elseif active_theme == "naysayer" then
+  return {
+    {
+      "RostislavArts/naysayer.nvim",
+      name = "naysayer",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.cmd("colorscheme naysayer")
+      end,
+    },
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "naysayer",
+      },
+    },
+  }
+else
+  error("Unknown theme: " .. active_theme)
+end

92c59c0ccf2ec3c31625d620af3cb9e814ac3666
MarioMottl <mario.mottl@pm.me>
2025-07-25 16:08:39 +0200

    update plugins

--- lua/plugins/colorsheme.lua
+++ lua/plugins/colorsheme.lua
@@ -4,44 +4,44 @@ local active_theme = "naysayer"
 local kanso_variant = "zen"
 
 if active_theme == "kanso" then
-  return {
-    {
-      "webhooked/kanso.nvim",
-      name = "kanso",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        require("kanso").setup({
-          theme = kanso_variant,
-        })
-        vim.cmd("colorscheme kanso-" .. kanso_variant)
-      end,
-    },
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "kanso-" .. kanso_variant,
-      },
-    },
-  }
+    return {
+        {
+            "webhooked/kanso.nvim",
+            name = "kanso",
+            lazy = false,
+            priority = 1000,
+            config = function()
+                require("kanso").setup({
+                    theme = kanso_variant,
+                })
+                vim.cmd("colorscheme kanso-" .. kanso_variant)
+            end,
+        },
+        {
+            "LazyVim/LazyVim",
+            opts = {
+                colorscheme = "kanso-" .. kanso_variant,
+            },
+        },
+    }
 elseif active_theme == "naysayer" then
-  return {
-    {
-      "RostislavArts/naysayer.nvim",
-      name = "naysayer",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        vim.cmd("colorscheme naysayer")
-      end,
-    },
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "naysayer",
-      },
-    },
-  }
+    return {
+        {
+            "RostislavArts/naysayer.nvim",
+            name = "naysayer",
+            lazy = false,
+            priority = 1000,
+            config = function()
+                vim.cmd("colorscheme naysayer")
+            end,
+        },
+        {
+            "LazyVim/LazyVim",
+            opts = {
+                colorscheme = "naysayer",
+            },
+        },
+    }
 else
-  error("Unknown theme: " .. active_theme)
+    error("Unknown theme: " .. active_theme)
 end

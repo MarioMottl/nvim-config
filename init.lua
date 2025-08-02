b14d448083e88f283ccc5cc7e4eecf32df148d7a
MarioMottl <mario.mottl@pm.me>
2025-08-02 23:38:55 +0200

    Add switch between `normal` and `absolute` numbers in Normal / Insert Mode

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.opt.clipboard = "unnamedplus"
 vim.o.number = true
@@ -6,3 +5,22 @@ vim.o.relativenumber = false
 vim.opt.list = true
 vim.keymap.set("x", "p", '"_dP', { noremap = true, silent = true })
 vim.keymap.set("x", "P", '"_dP', { noremap = true, silent = true })
+
+-- Normal mode relative numbers insert mode absolute numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+local number_toggle = vim.api.nvim_create_augroup("NumberToggle", { clear = true })
+vim.api.nvim_create_autocmd("InsertEnter", {
+    group = number_toggle,
+    pattern = "*",
+    callback = function()
+        vim.opt.relativenumber = false
+    end,
+})
+vim.api.nvim_create_autocmd("InsertLeave", {
+    group = number_toggle,
+    pattern = "*",
+    callback = function()
+        vim.opt.relativenumber = true
+    end,
+})

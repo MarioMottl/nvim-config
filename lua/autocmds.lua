a8b70aa48cf8c85579dd4d6a6140730d781d96be
Mario Mottl <mario.mottl@pm.me>
2025-11-14 16:35:47 +0100

    enable different line number styles depending on mode

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,25 @@
 require "nvchad.autocmds"
+
+-- Normal Mode -> relative line numbers | Insert Mode -> absolute line numbers
+vim.opt.number = true
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
+  pattern = "*",
+  callback = function()
+    if vim.wo.number and vim.api.nvim_get_mode().mode ~= "i" then
+      vim.wo.relativenumber = true
+    end
+  end,
+})
+
+autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
+  pattern = "*",
+  callback = function()
+    if vim.wo.number then
+      vim.wo.relativenumber = false
+    end
+  end,
+})
+
